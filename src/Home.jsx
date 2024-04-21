edd93b563ca4d3e61c430e6ec1ccbd2c6dde0863
Dariusz Okoński <darek200180@gmail.com>
2024-04-21 11:52:18 +0200

    add cotext

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useGlobalContext } from './context';
 
 const Home = () => {
+  const { openSidebar, openModal } = useGlobalContext();
+
   return <div>Home</div>;
 };
 
---
+++ src/context.jsx
@@ -0,0 +1,48 @@
+import { createContext, useContext, useState } from 'react';
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModalOpen,
+        openSidebar,
+        closeSidebar,
+        openModal,
+        closeModal,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error('Context is out of Provider');
+  }
+
+  return context;
+};
